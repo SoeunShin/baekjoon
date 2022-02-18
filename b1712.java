eb83ad67c6939a43540bacb95c9c9bae212eef45
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-02-18 15:56:46 +0900

    220217

--- b1712.java
+++ b1712.java
@@ -8,15 +8,15 @@ public class b1712 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
 		
-		if (c <= b) {
+		if (C <= B) {
 			System.out.println(-1);
 		}
 		else {
-			System.out.println(a/(c-b)+1);
+			System.out.println(A/(C-B)+1);
 		}
 	}
 }
