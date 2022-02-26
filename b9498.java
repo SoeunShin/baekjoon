d759172ad4607cb5e238c7d97c72d207ce565836
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-02-26 23:54:57 +0900

    220225

--- b9498.java
+++ b9498.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class b9498 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int score = sc.nextInt();
 
 		if(score >= 90) System.out.println("A");
