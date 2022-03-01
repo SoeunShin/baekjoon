255c0d50b462db6c38e987ba8c2e7353005e40fd
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-01 23:45:37 +0900

    220301

---
+++ b2741.java
@@ -0,0 +1,16 @@
+package java220301;
+
+// 백준 2741. N 찍기 
+import java.util.Scanner;
+
+public class b2741 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(i+1);
+		}
+	}
+}
