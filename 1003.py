02c7c85747309d47e3b761b21d0712a57ee0e5df
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-09 23:54:51 +0900

    220309

---
+++ 1003.py
@@ -0,0 +1,13 @@
+# 백준 1003. 피보나치 함수 
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    zero = 1
+    one = 0
+    tmp = 0
+    for _ in range(n):
+        tmp = one
+        one = one + zero
+        zero = tmp
+    print(zero, one)
