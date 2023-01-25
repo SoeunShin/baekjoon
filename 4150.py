d1c9bf8b79f82587c9f07fbed30fd8f4ae0f26cb
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2023-01-25 10:38:51 +0900

    230125

---
+++ 4150.py
@@ -0,0 +1,27 @@
+# 백준
+# 4150. 피보나치 수
+
+"""
+import sys
+
+def fibo(num):
+    if num == 0:
+        return 0
+    elif num == 1 or num == 2:
+        return 1
+    else:
+        return fibo(num-1) + fibo(num-2)
+
+print(fibo(int(sys.stdin.readline())))
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+
+fibo = [0,1,1]
+
+for i in range(3, n+1):
+    fibo.append(fibo[i-1] + fibo[-2])
+
+print(fibo[n])
